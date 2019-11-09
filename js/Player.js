5725c0d573d28fa688b7fbd2ac17abadda0f2086
salientype <jorge@salientype.com>
2019-11-09 01:03:10 -0500

    created function to get a pool of skills

--- js/Player.js
+++ js/Player.js
@@ -14,10 +14,10 @@ class Player {
     handleErrors(response) {
 
         if (!response.ok) {
-            console.log('The raven from Westoros never came...');
+            console.log('The raven from Westeros never came...');
             throw Error(response.statusText);
         }
-        
+
         return response;
 
     }
@@ -25,10 +25,52 @@ class Player {
     getSkillsPool = () => {
 
         fetch('https://pokeapi.co/api/v2/move-category/0/')
-
             .then(this.handleErrors)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => { 
+                
+                let skillsPool = [];
+
+                function getRandomInt(min, max) {
+        
+                    min = Math.ceil(min);
+                    max = Math.floor(max);
+                    return Math.floor(Math.random() * (max - min + 1)) + min;
+            
+                }
+                
+                function killSkillDuplicate(skillFromPool, newSkill) {
+
+                    if (skillFromPool == newSkill) {
+
+                        newSkill = data.moves[getRandomInt(0, 331)];
+                        killSkillDuplicate(skillFromPool, newSkill);
+
+                    } else {
+
+                        return newSkill;
+
+                    }
+
+                }
+
+                for(let i = 0; i < 40; i++) {
+                    
+                    let skill = data.moves[getRandomInt(0, 331)];
+
+                    skillsPool.forEach(function(obj) {
+                        
+                        killSkillDuplicate(obj, skill);
+                    
+                    });
+
+                    skillsPool.push(skill);
+
+                }
+
+                console.log(skillsPool);
+        
+            });
 
     }
 
