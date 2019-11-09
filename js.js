195d1fd3de1b00a42c7fdecfbd080586f8e81747
Rhonda Gilliard <amourg08@gmail.com>
2019-11-09 13:43:47 -0500

    update to css

---
+++ js.js
@@ -0,0 +1,22 @@
+
+
+//   const VIDLIST = [
+//     'https://www.youtube.com/watch?v=daDL7AcaPM0',
+//     'https://www.youtube.com/watch?v=jiaA25bM3qY',
+//     'https://www.youtube.com/watch?v=ftPa5km_T9A',
+//     'https://www.youtube.com/watch?v=xKSPffOFBX0',
+//     'https://www.youtube.com/watch?v=NspqGM0DbbQ'
+//   ];
+
+
+//   const video1 = axios.get('https://www.youtube.com/watch?v=daDL7AcaPM0');
+//   const video2 = axios.get('https://www.youtube.com/watch?v=jiaA25bM3qY');
+//   const video3 = axios.get('https://www.youtube.com/watch?v=ftPa5km_T9A');
+//   const video4 = axios.get('https://www.youtube.com/watch?v=xKSPffOFBX0');
+//   const video5 = axios.get('https://www.youtube.com/watch?v=NspqGM0DbbQ');
+  
+//   Promise.all([video1, video2, video3, video4, video5])
+//    .then((responses) => {
+//        console.log("I see them now!");
+//        console.log(responses)
+//    });
