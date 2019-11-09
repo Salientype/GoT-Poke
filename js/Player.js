cd3cb95ad4b69a71a5edbc6a18359ea4305ff030
Jorge Alvarez <jorgealvarez@Jorges-MacBook-Pro.local>
2019-11-09 13:59:12 -0500

    added Html table for skills pool

--- js/Player.js
+++ js/Player.js
@@ -27,19 +27,45 @@ class Player {
         fetch('https://pokeapi.co/api/v2/move-damage-class/2/')
             .then(this.handleErrors)
             .then(response => response.json())
-            .then(data => { 
-                
+            .then(data => {
+
                 let skillsPool = [];
                 let tempArray = [];
+                
+                const skillsPoolTable = document.createElement('table');
+                const skillsPoolTableSetup = `
+                <thead>
+                    <tr>
+                        <th scope="col">Skill Name</th>
+                        <th scope="col">Power</th>
+                        <th scope="col">Mana</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                    
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                    
+                    </tr>
+                </tbody>
+                `
+                skillsPoolTable.innerHTML = skillsPoolTableSetup;
 
                 function getRandomInt(min, max) {
-        
+
                     min = Math.ceil(min);
                     max = Math.floor(max);
                     return Math.floor(Math.random() * (max - min + 1)) + min;
-            
+
                 }
-                
+
                 function killSkillDuplicate(skillFromPool, newSkill) {
 
                     if (skillFromPool.url == newSkill.url) {
@@ -55,35 +81,43 @@ class Player {
 
                 }
 
-                for(let i = 0; i < 28; i++) {
-                    
+                for (let i = 0; i < 28; i++) {
+
                     let newSkill = data.moves[getRandomInt(0, 293)];
 
-                    skillsPool.forEach(function(skill) {
-                        
+                    skillsPool.forEach(function (skill) {
+
                         killSkillDuplicate(skill, newSkill);
-                    
+
                     });
 
                     skillsPool.push(newSkill);
 
                 }
 
-                skillsPool.forEach(function(skill) {
-                    
+                skillsPool.forEach(function (skill) {
+
                     fetch(skill.url)
                         .then(globalThis.handleErrors)
                         .then(response => response.json())
-                        .then(data => { 
+                        .then(data => {
+
                             tempArray.push(data);
+
                         });
 
-                    skillsPool = tempArray;
+                });
+
+                skillsPool = tempArray;
+
+                skillsPool.forEach(function (skill) {
+
+                    skillsPoolTable.appendChild
 
                 });
 
                 console.log(skillsPool);
-        
+
             });
 
     }
