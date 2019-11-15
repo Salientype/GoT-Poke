45cabefe20a7940679b477c144c7fddb091369ae
Jorge Alvarez <jorgealvarez@Jorges-MacBook-Pro.local>
2019-11-14 22:34:28 -0500

    fetch data now returns 4 unique skills from array

--- js/Player.js
+++ js/Player.js
@@ -10,7 +10,7 @@ class Player {
 
     }
 
-    static getRandomInt(min, max) {
+    getRandomInt(min, max) {
 
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -18,7 +18,7 @@ class Player {
 
     }
 
-    static handleErrors(response) {
+    handleErrors(response) {
 
         if (!response.ok) {
 
@@ -31,27 +31,27 @@ class Player {
 
     }
 
-
-    static killSkillDuplicate(skillFromPool, newSkill) {
-
-        if (skillFromPool == newSkill) {
-
-            newSkill = getSkill();
-            return killSkillDuplicate(skillFromPool, newSkill);
-
-        } else {
-
-            return newSkill;
-
-        }
-
-    }
-    
     async fetchData(url, callback) {
 
-        const handleErrors = this.handleErrors;
         const getRandomInt = this.getRandomInt;
+        const handleErrors = this.handleErrors;
         let skillsArray = [];
+        let playerSkills = [];
+        
+        function killSkillDuplicate(skillFromPool, newSkill) {
+
+            if (skillFromPool == newSkill) {
+    
+                newSkill = skillsArray[getRandomInt(0, skillsArray.length)]
+                return killSkillDuplicate(skillFromPool, newSkill);
+    
+            } else {
+    
+                return newSkill;
+    
+            }
+    
+        }
 
         await fetch(url)  // return fetch here
             .then(handleErrors)
@@ -81,122 +81,37 @@ class Player {
 
                     setTimeout ( function() { 
                         
-                        console.log(skillsArray); 
-                
+                        //console.log(skillsArray);
+                        
+                        for (let i = 0; i < 4; i++) {
+                            
+                            let selectedSkill = skillsArray[getRandomInt(0, skillsArray.length)];
+
+                            if (playerSkills.length != 0) {
+
+                                playerSkills.forEach(function (skill) {
+        
+                                    killSkillDuplicate(skill, selectedSkill);
+        
+                                })
+        
+                                playerSkills.push(selectedSkill);
+        
+                            } else {
+        
+                                playerSkills.push(selectedSkill);
+        
+                            }
+
+                        }
+
+                        console.log(playerSkills);
+
                     }, 4000);
 
             });
 
     }
-
-    getSkillsPool = () => {
-
-        let skillsPool = [];
-        let tempArray = [];
-        const getRandomInt = this.getRandomInt;
-        const handleErrors = this.handleErrors;
-
-        fetch('https://pokeapi.co/api/v2/move-category/0/')
-
-            .then(handleErrors)
-            .then(response => response.json())
-            .then(data => {
-
-                function getSkill() {
-
-                    return data.moves[getRandomInt(0, (data.moves.length - 1))];
-
-                }
-
-                function killSkillDuplicate(skillFromPool, newSkill) {
-
-                    if (skillFromPool == newSkill) {
-
-                        newSkill = getSkill();
-                        return killSkillDuplicate(skillFromPool, newSkill);
-
-                    } else {
-
-                        return newSkill;
-
-                    }
-
-                }
-
-                for (let i = 0; i < 100; i++) {
-
-                    let newSkill = getSkill();
-
-                    if (skillsPool.length != 0) {
-
-                        skillsPool.forEach(function (skill) {
-
-                            newSkill = killSkillDuplicate(skill, newSkill);
-
-                        })
-
-                        skillsPool.push(newSkill);
-
-                    } else {
-
-                        skillsPool.push(newSkill);
-
-                    }
-
-                }
-
-                console.log(skillsPool);
-
-                skillsPool.forEach(function (skill, index) {
-
-                    tempArray.push(index[skill]);
-
-                });
-
-            });
-
-        return skillsPool;
-    }
-
-    getFinalSkills = () => {
-
-        const skillsPool = [];
-        const skillsArray = [];
-        const handleErrors = this.handleErrors;
-
-        // console.log(skillsPool);
-
-        skillsPool.forEach(function (skill) {
-
-            fetch(skill.url)
-
-                .then(handleErrors(response))
-                .then(response => response.json())
-                .then(data => {
-
-                    if (data.accuracy != null &&
-                        data.accuracy != 100 &&
-                        data.power != null &&
-                        data.pp != null &&
-                        data.target.name == 'selected-pokemon') {
-
-                        skillsArray.push({ name: data.names[2].name, accuracy: data.accuracy, power: data.power, pp: data.pp });
-
-                    } else {
-
-                        skillsArray.push({ name: data.names[2].name, accuracy: data.accuracy, power: data.power, pp: data.pp });
-
-                    }
-
-                });
-
-        });
-
-        return skillsArray;
-
-    };
-
-
 
     skill1 = (array) => {
 
