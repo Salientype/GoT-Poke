4b7b7ec5245c2f04da9305f0ef80b94e6f2a369e
salientype <jorge@salientype.com>
2019-11-16 19:54:34 -0500

    random attack skills now reload into HTML on game restart

--- js/script.js
+++ js/script.js
@@ -1,40 +1,44 @@
-const skillsPoolTable = document.createElement('table');
-
-const skillsPoolTableSetup =`
-<thead>
-    <tr>
-        <th scope="col">Skill Name</th>
-        <th scope="col">Power</th>
-        <th scope="col">Mana</th>
-    </tr>
-</thead>
-<tbody>
-    <tr>
-        <th scope="row">1</th>
-    
-    </tr>
-    <tr>
-        <th scope="row">2</th>
-
-    </tr>
-    <tr>
-        <th scope="row">3</th>
-    
-    </tr>
-</tbody>
-`;
-
-skillsPoolTable.innerHTML = skillsPoolTableSetup;
-let skills;
-
 const playerHuman = new Player('Frodo', 'hobbit', 'male');
+const playerMachine = new Player('Frodo', 'hobbit', 'male');
 
-playerHuman.fetchData('https://pokeapi.co/api/v2/move-category/0/', function(playerSkills) {
+function addPlayerSkills(player, playerSkills, skillsContainer) {
 
-    skills = playerSkills;
-    console.log(skills);
+    player.skill_1 = playerSkills[0];
+    player.skill_2 = playerSkills[1];
+    player.skill_3 = playerSkills[2];
+    player.skill_4 = playerSkills[3];
 
-});
+    skillsContainer.innerHTML = `
+    <button type="button" class="btn btn-outline-primary btn-md">${player.skill_1.name}</br>${player.skill_1.power}/${player.skill_1.pp}/${player.skill_1.accuracy}</button>
+    <button type="button" class="btn btn-outline-primary btn-md">${player.skill_2.name}</br>${player.skill_2.power}/${player.skill_2.pp}/${player.skill_2.accuracy}</button>
+    <button type="button" class="btn btn-outline-primary btn-md">${player.skill_3.name}</br>${player.skill_3.power}/${player.skill_3.pp}/${player.skill_3.accuracy}</button>
+    <button type="button" class="btn btn-outline-primary btn-md">${player.skill_4.name}</br>${player.skill_4.power}/${player.skill_4.pp}/${player.skill_4.accuracy}</button>
+    <button type="button" class="btn btn-outline-primary btn-md">RUN</button>
+    `
+
+}
+
+function getPlayerSkills() {
+
+    playerHuman.fetchData('https://pokeapi.co/api/v2/move-category/0/', function (playerSkills) {
+
+        let skillsContainer = document.getElementById('human-player-skills');
+        addPlayerSkills(playerHuman, playerSkills, skillsContainer);
+
+    });
+
+    playerMachine.fetchData('https://pokeapi.co/api/v2/move-category/0/', function (playerSkills) {
+
+        let skillsContainer = document.getElementById('machine-player-skills');
+        addPlayerSkills(playerMachine, playerSkills, skillsContainer);
+
+    });
+
+}
+
+
+//setTimeout(function(){ console.log(playerHuman.skill_1); }, 5000);
+
 
 
 
